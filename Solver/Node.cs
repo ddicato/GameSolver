334884100ca6120619dcefe32bda2e3d59b8a791
Darb <daavan42@gmail.com>
2012-02-03 18:19:12 -0800

    Stubbed out GraphLib

--- Solver/Node.cs
+++ Solver/Node.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 
 namespace Solver {
-    public abstract class Node<T> where T : Node<T> {
+    public abstract class Node<Self> where Self : Node<Self> {
         // TODO: rename to Equator
-        public abstract IEqualityComparer<T> Comparator { get; }
+        public abstract IEqualityComparer<Self> Comparator { get; }
         public abstract bool IsWinning { get; }
-        public abstract List<T> GetChildren();
+        public abstract List<Self> GetChildren();
 
         public abstract override String ToString();
     }
