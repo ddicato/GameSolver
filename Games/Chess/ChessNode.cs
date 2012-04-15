09137661895a3f6b3fb176e2f2785160eb0aa7bd
ddicato <daavan42@gmail.com>
2012-04-15 00:28:48 -0700

    Partial playbook and chess engine

---
+++ Games/Chess/ChessNode.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Solver;
+using System.Diagnostics;
+
+namespace Chess {
+    public class ChessNode : TwoPlayerNode<ChessNode> {
+        private const int WHITE = 0;
+        private const int BLACK = 1;
+
+        private uint data;
+
+        private List<Coord> whitePawns;
+        private List<Coord> whiteKnights;
+        private List<Coord> whiteBishops;
+        private List<Coord> whiteRooks;
+        private List<Coord> whiteQueens;
+        private Coord whiteKing;
+
+        private List<Coord> blackPawns;
+        private List<Coord> blackKnights;
+        private List<Coord> blackBishops;
+        private List<Coord> blackRooks;
+        private List<Coord> blackQueens;
+        private Coord blackKing;
+
+        public ChessNode() {
+
+        }
+
+        private bool BoolGetData(uint mask) {
+            return (this.data & mask) != 0;
+        }
+
+        private void BoolSetData(bool value, uint mask) {
+            if (value) {
+                this.data |= mask;
+            } else {
+                this.data &= ~mask;
+            }
+        }
+
+        private uint GetData(uint mask) {
+            return this.data & mask;
+        }
+
+        private uint GetData(uint mask, int shift) {
+            return (this.data & (mask << shift)) >> shift;
+        }
+
+        private void SetData(uint value, uint mask) {
+            this.data = (this.data & ~mask) | (value & mask);
+        }
+
+        private void SetData(uint value, uint mask, int shift) {
+            this.data = (this.data & ~(mask << shift)) | ((value & mask) << shift);
+        }
+
+        public override int Turn {
+            get { return (int)this.GetData(1u, 20); }
+            private set { this.SetData((uint)value, 1u, 20); }
+        }
+
+        public bool WhiteCastleKing {
+            get { return this.BoolGetData(1u << 16); }
+            private set { this.BoolSetData(value, 1u << 16); }
+        }
+
+        public bool WhiteCastleQueen {
+            get { return this.BoolGetData(1u << 17); }
+            private set { this.BoolSetData(value, 1u << 17); }
+        }
+
+        public bool BlackCastleKing {
+            get { return this.BoolGetData(1u << 18); }
+            private set { this.BoolSetData(value, 1u << 18); }
+        }
+
+        public bool BlackCastleQueen {
+            get { return this.BoolGetData(1u << 19); }
+            private set { this.BoolSetData(value, 1u << 19); }
+        }
+
+        public bool EnPassantPossible {
+            get { return this.BoolGetData(0x0800u); }
+            private set { this.BoolSetData(value, 0x0800u); }
+        }
+
+        public uint EnPassantFile {
+            get { return this.GetData(0x07u, 8); }
+            private set { this.SetData(value, 0x07u, 8); }
+        }
+
+        #region Move tables
+
+        static ChessNode() {
+            CoordOffset[] WhitePawnMoves = new CoordOffset[] {
+                new CoordOffset(0, 1)
+            };
+            CoordOffset[] WhitePawnDoubleMoves = new CoordOffset[] {
+                new CoordOffset(0, 2)
+            };
+            CoordOffset[] WhitePawnCaptures = new CoordOffset[] {
+                new CoordOffset(-1, 1),
+                new CoordOffset(1, 1)
+            };
+            CoordOffset[] BlackPawnMoves = new CoordOffset[] {
+                new CoordOffset(0, -1)
+            };
+            CoordOffset[] BlackPawnDoubleMoves = new CoordOffset[] {
+                new CoordOffset(0, -1)
+            };
+            CoordOffset[] BlackPawnCaptures = new CoordOffset[] {
+                new CoordOffset(-1, -1),
+                new CoordOffset(1, -1)
+            };
+
+            CoordOffset[] KnightMoves = new CoordOffset[] {
+                new CoordOffset(-2, 1),
+                new CoordOffset(-1, 2),
+                new CoordOffset(1, 2),
+                new CoordOffset(2, 1),
+                new CoordOffset(2, -1),
+                new CoordOffset(1, -2),
+                new CoordOffset(-1, -2),
+                new CoordOffset(-2, -1)
+            };
+
+            CoordOffset[][] Diagonals = new CoordOffset[][] {
+                new CoordOffset[] {
+                    new CoordOffset(-1, 1),
+                    new CoordOffset(-2, 2),
+                    new CoordOffset(-3, 3),
+                    new CoordOffset(-4, 5),
+                    new CoordOffset(-5, 6),
+                    new CoordOffset(-6, 6),
+                    new CoordOffset(-7, 7)
+                },
+                new CoordOffset[] {
+                    new CoordOffset(1, 1),
+                    new CoordOffset(2, 2),
+                    new CoordOffset(3, 3),
+                    new CoordOffset(4, 4),
+                    new CoordOffset(5, 5),
+                    new CoordOffset(6, 6),
+                    new CoordOffset(7, 7),
+                },
+                new CoordOffset[] {
+                    new CoordOffset(1, -1),
+                    new CoordOffset(2, -2),
+                    new CoordOffset(3, -3),
+                    new CoordOffset(4, -4),
+                    new CoordOffset(5, -5),
+                    new CoordOffset(6, -6),
+                    new CoordOffset(7, -7),
+                },
+                new CoordOffset[] {
+                    new CoordOffset(-1, -1),
+                    new CoordOffset(-2, -2),
+                    new CoordOffset(-3, -3),
+                    new CoordOffset(-4, -4),
+                    new CoordOffset(-5, -5),
+                    new CoordOffset(-6, -6),
+                    new CoordOffset(-7, -7),
+                }
+            };
+        }
+
+
+
+        private static readonly uint[] PawnMoves;
+
+        private struct MoveTable {
+            private readonly uint[] Moves;
+            
+            public MoveTable() {
+                this.Moves = new uint[64];
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The coordinate of the square that is capturable via en passant, if any.
+        /// </summary>
+        public Coord EnPassantCoord {
+            // If it's white's turn, then the capture square is on black's side, and vice-versa.
+            get { return new Coord(this.EnPassantFile, this.Turn == WHITE ? 5u : 2u); }
+        }
+
+        public uint FiftyMoveRule {
+            get { return this.GetData(0xffu); }
+            private set { this.SetData(value, 0xffu); }
+        }
+
+        public override bool IsGameOver {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override List<ChessNode> GetChildren() {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(ChessNode other) {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode() {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString() {
+            throw new NotImplementedException();
+        }
+
+        public enum ChessPiece {
+            Pawn,
+            Knight,
+            Bishop,
+            Rook,
+            Queen,
+            King
+        }
+
+        public struct Coord {
+            public static Coord InvalidCoord = new Coord();
+
+            public uint Loc;
+
+            // constructor that creates an invalid coordinate
+            private Coord() {
+                this.Loc = unchecked((uint)-1);
+            }
+
+            public Coord(uint loc) {
+                Debug.Assert(loc < 64);
+                this.Loc = loc;
+            }
+
+            public Coord(uint file, uint rank) {
+                Debug.Assert(file < 8 && rank < 8);
+                this.Loc = file + (rank * 8);
+            }
+
+            public static explicit operator uint(Coord coord) {
+                return coord.Loc;
+            }
+
+            public static explicit operator Coord(uint loc) {
+                return new Coord(loc);
+            }
+
+            public bool IsValid {
+                get { return this.Loc < 64; }
+            }
+
+            public uint File {
+                get {
+                    return this.Loc & 7u; // equivalent to this.Loc % 8
+                }
+            }
+
+            public uint Rank {
+                get {
+                    return this.Loc / 8u;
+                }
+            }
+        }
+
+        private struct CoordOffset {
+            public int Offset;
+
+            public CoordOffset(int fileOffset, int rankOffset) {
+                this.Offset = fileOffset + (rankOffset * 8);
+            }
+        }
+    }
+}
---
+++ Solver/Playbook.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solver {
+    public enum EntryType {
+        NonLeaf,
+        Win,
+        Loss,
+        Draw
+    }
+
+    public class PlaybookEntry<T> {
+        public List<PlaybookEntry<T>> parents;
+
+        // Paired with the transformation that makes
+        private List<KeyValuePair<PlaybookEntry<T>, int>> children;
+
+        public Playbook<T> Playbook {
+            get;
+            private set;
+        }
+
+        public T State {
+            get;
+            private set;
+        }
+
+        public int ExpectedScore {
+            get;
+            private set;
+        }
+
+        public ulong GamesPlayed {
+            get;
+            private set;
+        }
+
+        private PlaybookEntry() {
+            throw new InvalidOperationException();
+        }
+
+        public PlaybookEntry(Playbook<T> playbook, T state, int score) {
+            this.Playbook = playbook;
+            this.State = state;
+            this.ExpectedScore = score;
+            this.GamesPlayed = 1ul;
+        }
+
+        public int ParentCount {
+            get {
+                return this.parents == null ? 0 : this.parents.Count;
+            }
+        }
+
+        public int ChildCount {
+            get {
+                return this.children == null ? 0 : this.children.Count;
+            }
+        }
+
+        /* TODO: store hashset of entries instead of dict mapping state to entry
+        public override bool Equals(object obj) {
+            var other = obj as PlaybookEntry<T>;
+            return other != null && this.State.Equals(other.State);
+        }
+
+        public override int GetHashCode() {
+            return this.State.GetHashCode();
+        }
+        */
+    }
+
+    public class Playbook<T> {
+        private readonly Tuple<Func<T, T>, Func<T, T>>[] Transforms;
+        private readonly Dictionary<T, PlaybookEntry<T>> States;
+
+        public Playbook(Tuple<Func<T, T>, Func<T, T>>[] transforms = null) {
+            if (transforms == null || transforms.Length == 0) {
+                this.Transforms = new Tuple<Func<T, T>, Func<T, T>>[] {
+                    new Tuple<Func<T, T>, Func<T, T>>(state => state, state => state)
+                };
+            } else {
+                this.Transforms = transforms;
+            }
+
+            this.States = new Dictionary<T, PlaybookEntry<T>>();
+        }
+
+        private bool IsEquivalent(T a, T b) {
+            for (int i = 0; i < this.Transforms.Length; i++) {
+                if (a.Equals(this.Transform(i, b))) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsEquivalent(T a, T b, out int transformIndex) {
+            for (int i = 0; i < this.Transforms.Length; i++) {
+                if (a.Equals(this.Transform(i, b))) {
+                    transformIndex = i;
+                    return true;
+                }
+            }
+
+            transformIndex = -1;
+            return false;
+        }
+
+        public T Transform(int index, T state) {
+            return this.Transforms[index].Item1(state);
+        }
+
+        public T InverseTransform(int index, T state) {
+            return this.Transforms[index].Item2(state);
+        }
+
+        public bool Contains(T state) {
+            for (int i = 0; i < this.Transforms.Length; i++) {
+                if (this.States.ContainsKey(this.Transform(i, state))) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryGetValue(T state, out PlaybookEntry<T> entry) {
+            entry = null;
+            for (int i = 0; i < this.Transforms.Length; i++) {
+                if (this.States.TryGetValue(this.Transform(i, state), out entry)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Remove(T state) {
+            bool removed = false;
+            for (int i = 0; i < this.Transforms.Length; i++) {
+                removed |= this.States.Remove(this.Transform(i, state));
+            }
+
+            return removed;
+        }
+
+        public PlaybookEntry<T> this[T state] {
+            get {
+                PlaybookEntry<T> entry;
+                if (this.TryGetValue(state, out entry)) {
+                    return entry;
+                }
+
+                return null;
+            }
+            set {
+                if (value == null) {
+                    this.Remove(state);
+                } else {
+                    PlaybookEntry<T> entry = this[state];
+                    if (state != null) {
+                        this.States[entry.State] = entry;
+                    }
+                }
+            }
+        }
+    }
+}
